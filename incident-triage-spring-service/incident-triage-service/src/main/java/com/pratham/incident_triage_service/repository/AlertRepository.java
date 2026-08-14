1f79623bd42c1412dcd11166b8e1812ad052ecd3
Pratham Ahuja <ahujapa05@gmail.com>
2026-08-14 22:31:59 +0530

    Revert "Added controller and repository layer"
    This reverts commit 29284795ecb35c2d62a49e2e1ea2d1efeb22a450.

--- incident-triage-spring-service/incident-triage-service/src/main/java/com/pratham/incident_triage_service/repository/AlertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pratham.incident_triage_service.repository;
-
-import com.pratham.incident_triage_service.entity.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AlertRepository extends JpaRepository<Alert, Long> {
-}
